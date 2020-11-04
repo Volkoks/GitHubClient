9b71fb8a0701c4a39e6dec66c6c2a5b20e9c2a8e
Volkoks <volk-2390@gmail.com>
2020-11-04 22:20:14 +0300

    В прошлом коммите случайно добавил класс MainActivity.kt в коммит - в данном коммите добавил описание

--- app/src/main/java/com/example/githubclient/MainActivity.kt
+++ app/src/main/java/com/example/githubclient/MainActivity.kt
@@ -6,7 +6,9 @@ import com.example.githubclient.presenter.Presenter
 import com.example.githubclient.view.ForViewAndModel
 import kotlinx.android.synthetic.main.activity_main.*
 
-
+/**
+ * Главная View данного приложения в котором обрабатывается взаимодействие пользователя и приложения
+ */
 class MainActivity : AppCompatActivity(), ForViewAndModel.forView {
 
     private val presenter: Presenter? by lazy {
@@ -24,6 +26,9 @@ class MainActivity : AppCompatActivity(), ForViewAndModel.forView {
 
     }
 
+    /**
+     * Здесь получаем данные от Presenter
+     */
     override fun setBtn1(value: Int) {
         btn_counter_1.text = "Количество = $value"
     }
