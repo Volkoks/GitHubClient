1f3fa7507001fccb4c403d3fd798408654e434da
Volkoks <volk-2390@gmail.com>
2020-11-09 13:31:39 +0300

    удалил ненужные библиотеки

--- app/src/main/java/com/example/githubclient/navigator/Screens.kt
+++ app/src/main/java/com/example/githubclient/navigator/Screens.kt
@@ -1,6 +1,5 @@
 package com.example.githubclient.navigator
 
-import androidx.fragment.app.Fragment
 import com.example.githubclient.mvp.model.entity.GitHubUser
 import com.example.githubclient.ui.fragments.UserFragment
 import com.example.githubclient.ui.fragments.UsersFragment
