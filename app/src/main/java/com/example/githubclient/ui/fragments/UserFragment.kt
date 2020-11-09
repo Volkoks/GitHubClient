4c9d6111e3b45462e286f9a71ff4403ad3dbee2d
Volkoks <volk-2390@gmail.com>
2020-11-09 13:28:47 +0300

    Сделал добавление в Bundle данных при создании фрагмента

--- app/src/main/java/com/example/githubclient/ui/fragments/UserFragment.kt
+++ app/src/main/java/com/example/githubclient/ui/fragments/UserFragment.kt
@@ -7,15 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.githubclient.R
 import com.example.githubclient.mvp.model.entity.GitHubUser
+import kotlinx.android.synthetic.main.fragment_user.*
 
 
 class UserFragment : Fragment() {
     companion object{
-        fun newInstance():Fragment{
+        fun newInstance(user:GitHubUser):Fragment{
             val fragment = UserFragment()
-
+            var bundle = Bundle()
+            bundle.putParcelable("user",user)
+            fragment.arguments = bundle
             return fragment
         }
+        var user:GitHubUser? = null
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,5 +28,7 @@ class UserFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        user = arguments?.getParcelable("user")
+        user_name_tv.text = user?.login
     }
 }
