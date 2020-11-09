0e21b5cd335953a6fe37166d8c49fccc28bb708b
Volkoks <volk-2390@gmail.com>
2020-11-09 09:39:12 +0300

    Parcelable в классе данных

--- app/src/main/java/com/example/githubclient/mvp/model/entity/GitHubUser.kt
+++ app/src/main/java/com/example/githubclient/mvp/model/entity/GitHubUser.kt
@@ -1,6 +1,10 @@
 package com.example.githubclient.mvp.model.entity
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class GitHubUser(
     var login:String
-) {
+):Parcelable {
 }
