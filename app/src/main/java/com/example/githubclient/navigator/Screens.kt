df6f6916d9b58bb99ab34fe235f9c35ff426b5b1
Volkoks <volk-2390@gmail.com>
2020-11-09 12:39:37 +0300

    rename UserFragment.kt

--- app/src/main/java/com/example/githubclient/navigator/Screens.kt
+++ app/src/main/java/com/example/githubclient/navigator/Screens.kt
@@ -1,11 +1,10 @@
 package com.example.githubclient.navigator
 
-import androidx.fragment.app.Fragment
-import com.example.githubclient.ui.fragments.UserFragment
+import com.example.githubclient.ui.fragments.UsersFragment
 import ru.terrakok.cicerone.android.support.SupportAppScreen
 
 class Screens {
     class UsersScreen : SupportAppScreen() {
-        override fun getFragment() = UserFragment.newInstance()
+        override fun getFragment() = UsersFragment.newInstance()
     }
 }
--- app/src/main/java/com/example/githubclient/ui/fragments/UsersFragment.kt
+++ app/src/main/java/com/example/githubclient/ui/fragments/UsersFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.githubclient.GitHubApp
 import com.example.githubclient.R
@@ -13,16 +12,16 @@ import com.example.githubclient.mvp.presenter.UsersPresenter
 import com.example.githubclient.mvp.view.UserView
 import com.example.githubclient.ui.BackButtonListener
 import com.example.githubclient.ui.adapter.UserRVAdapter
-import kotlinx.android.synthetic.main.fragment_user.*
+import kotlinx.android.synthetic.main.fragment_users.*
 import moxy.MvpAppCompatFragment
 import moxy.ktx.moxyPresenter
 
 
-class UserFragment : MvpAppCompatFragment(), UserView, BackButtonListener {
+class UsersFragment : MvpAppCompatFragment(), UserView, BackButtonListener {
 
 
     companion object {
-        fun newInstance() = UserFragment()
+        fun newInstance() = UsersFragment()
     }
 
     val presenter: UsersPresenter by moxyPresenter {
@@ -37,14 +36,14 @@ class UserFragment : MvpAppCompatFragment(), UserView, BackButtonListener {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ) = View.inflate(context, R.layout.fragment_user, null)
+    ) = View.inflate(context, R.layout.fragment_users, null)
 
 
     override fun init() {
-        user_recycler_view.layoutManager =
+        users_recycler_view.layoutManager =
             LinearLayoutManager(context)
         adapter = UserRVAdapter(presenter.usersListPresenter)
-        user_recycler_view.adapter = adapter
+        users_recycler_view.adapter = adapter
 
     }
 
