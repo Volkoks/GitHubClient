98493e7d29ff5005347d75472802a29f028adcf9
Volkoks <volk-2390@gmail.com>
2020-11-09 10:20:29 +0300

    Добавил анатацию интерфейса MainView.kt

--- app/src/main/java/com/example/githubclient/mvp/view/MainView.kt
+++ app/src/main/java/com/example/githubclient/mvp/view/MainView.kt
@@ -1,8 +1,10 @@
 package com.example.githubclient.mvp.view
 
 import moxy.MvpView
-import moxy.viewstate.strategy.alias.AddToEndSingle
+import moxy.viewstate.strategy.AddToEndSingleStrategy
+import moxy.viewstate.strategy.StateStrategyType
 
-@AddToEndSingle
+
+@StateStrategyType(AddToEndSingleStrategy::class)
 interface MainView:MvpView {
 }
