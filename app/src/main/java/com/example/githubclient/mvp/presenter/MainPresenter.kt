0493418214c8a83ca046da249786428cecf086ae
Volkoks <volk-2390@gmail.com>
2020-11-09 11:15:28 +0300

    Поправил призентер MainActivity

--- app/src/main/java/com/example/githubclient/mvp/presenter/MainPresenter.kt
+++ app/src/main/java/com/example/githubclient/mvp/presenter/MainPresenter.kt
@@ -1,19 +1,18 @@
 package com.example.githubclient.mvp.presenter
 
-import com.example.githubclient.GitHubApp
+
 import com.example.githubclient.mvp.view.MainView
 import com.example.githubclient.navigator.Screens
 import moxy.MvpPresenter
 import ru.terrakok.cicerone.Router
 
-class MainPresenter: MvpPresenter<MainView>() {
 
-    private val router:Router = GitHubApp.getRouter()
-
+class MainPresenter(val router: Router) : MvpPresenter<MainView>() {
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
-
         router.replaceScreen(Screens.UsersScreen())
     }
-    fun backClicked(){ router.exit() }
+    fun backClicked(){
+        router.exit()
+    }
 }
