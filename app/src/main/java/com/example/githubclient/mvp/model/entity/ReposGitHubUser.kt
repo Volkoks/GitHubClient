e4e99eece805a55553eaf5a6485a2a007773a408
Volkoks <volk-2390@gmail.com>
2020-11-22 15:39:12 +0300

    Сущность репозитория пользователя

---
+++ app/src/main/java/com/example/githubclient/mvp/model/entity/ReposGitHubUser.kt
@@ -0,0 +1,18 @@
+package com.example.githubclient.mvp.model.entity
+
+import android.os.Parcelable
+import com.google.gson.annotations.Expose
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class ReposGitHubUser(
+    @Expose
+    val id: String? = null,
+    @Expose
+    val name: String? = null,
+    @Expose
+    val forks: Int? = null,
+    @Expose
+    val description: String? = null
+) : Parcelable {
+}
