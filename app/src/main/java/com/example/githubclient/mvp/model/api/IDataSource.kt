9a0e8d66711c39e26a07a64892b5e54bffa4533f
Volkoks <volk-2390@gmail.com>
2020-11-22 14:55:13 +0300

    Добавил фунцию получения репозиториев пользователя

--- app/src/main/java/com/example/githubclient/mvp/model/api/IDataSource.kt
+++ app/src/main/java/com/example/githubclient/mvp/model/api/IDataSource.kt
@@ -1,9 +1,11 @@
 package com.example.githubclient.mvp.model.api
 
 import com.example.githubclient.mvp.model.entity.GitHubUser
+import com.example.githubclient.mvp.model.entity.ReposGitHubUser
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Url
 
 interface IDataSource {
     @GET("/users")
@@ -11,4 +13,7 @@ interface IDataSource {
 
     @GET("/user/{login}")
     fun loadUser(@Path("login") login: String): Single<GitHubUser>
+
+    @GET
+    fun getReposUser(@Url url:String):Single<ReposGitHubUser>
 }
