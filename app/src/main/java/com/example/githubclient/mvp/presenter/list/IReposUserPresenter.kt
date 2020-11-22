510148bc5d9bcc44569051f2cd9a88b440e8cbe8
Volkoks <volk-2390@gmail.com>
2020-11-22 21:47:47 +0300

    Интерфейс взаимодействия для презентера листа репозиториев пользователя

---
+++ app/src/main/java/com/example/githubclient/mvp/presenter/list/IReposUserPresenter.kt
@@ -0,0 +1,6 @@
+package com.example.githubclient.mvp.presenter.list
+
+import com.example.githubclient.mvp.view.ReposUserItemView
+
+interface IReposUserPresenter:IListPresenter<ReposUserItemView> {
+}
