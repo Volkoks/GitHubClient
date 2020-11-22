dab7b22f918f4291e3c17b2ea3a4880c181b95ba
Volkoks <volk-2390@gmail.com>
2020-11-22 21:51:37 +0300

    Презентер для экрана пользователя содержащий класс призентер для листа репозиториев

--- app/src/main/java/com/example/githubclient/mvp/presenter/UserPresenter.kt
+++ app/src/main/java/com/example/githubclient/mvp/presenter/UserPresenter.kt
@@ -1,20 +1,57 @@
 package com.example.githubclient.mvp.presenter
 
+import com.example.githubclient.mvp.model.entity.GitHubUser
+import com.example.githubclient.mvp.model.entity.ReposGitHubUser
 import com.example.githubclient.mvp.model.repository.IGitHubUsersRepo
+import com.example.githubclient.mvp.presenter.list.IReposUserPresenter
+import com.example.githubclient.mvp.view.ReposUserItemView
 import com.example.githubclient.mvp.view.UserView
 import io.reactivex.rxjava3.core.Scheduler
 import moxy.MvpPresenter
 import ru.terrakok.cicerone.Router
 
 class UserPresenter(
+    val mainThreadScheduler: Scheduler,
+    val usersRepo: IGitHubUsersRepo,
     val router: Router
 ) :
     MvpPresenter<UserView>() {
 
+    class ReposUserPresenter : IReposUserPresenter {
+        var reposUserList = mutableListOf<ReposGitHubUser>()
+        override var itemClickListener: ((ReposUserItemView) -> Unit)? = null
+
+
+        override fun bindView(itemView: ReposUserItemView) {
+            val repos = reposUserList[itemView.pos]
+            repos.name?.let { itemView.setReposName(it) }
+        }
+
+        override fun getCount() = reposUserList.size
+    }
+
+    val reposUserPresenter = ReposUserPresenter()
+    var user: GitHubUser? = null
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
         viewState.init()
+        loadRepos()
+        viewState.updateList()
+    }
+
+    fun loadRepos() {
+        user?.reposUrl?.let {
+            usersRepo.getReposUser(it)
+                .observeOn(mainThreadScheduler)
+                .subscribe({repos->
+                    reposUserPresenter.reposUserList.clear()
+                    reposUserPresenter.reposUserList.addAll(repos)
+                    viewState.updateList()
+                },{error->
+                    viewState.showError(error)
+                })
+        }
     }
 
     fun backPressed(): Boolean {
