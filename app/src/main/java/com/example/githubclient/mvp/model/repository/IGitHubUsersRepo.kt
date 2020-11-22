fec0690333c5aa5127f68d4907c6925ba333cd7a
Volkoks <volk-2390@gmail.com>
2020-11-22 15:43:38 +0300

    Добавил в класс и интерфейс для работы с ретровитом функции получения репозиториев

--- app/src/main/java/com/example/githubclient/mvp/model/repository/IGitHubUsersRepo.kt
+++ app/src/main/java/com/example/githubclient/mvp/model/repository/IGitHubUsersRepo.kt
@@ -1,8 +1,10 @@
 package com.example.githubclient.mvp.model.repository
 
 import com.example.githubclient.mvp.model.entity.GitHubUser
+import com.example.githubclient.mvp.model.entity.ReposGitHubUser
 import io.reactivex.rxjava3.core.Single
 
 interface IGitHubUsersRepo {
     fun getUsers(): Single<List<GitHubUser>>
+    fun getReposUser(url:String): Single<List<ReposGitHubUser>>
 }
--- app/src/main/java/com/example/githubclient/mvp/model/repository/RetrofitGitHubUsersRepo.kt
+++ app/src/main/java/com/example/githubclient/mvp/model/repository/RetrofitGitHubUsersRepo.kt
@@ -1,8 +1,11 @@
 package com.example.githubclient.mvp.model.repository
 
 import com.example.githubclient.mvp.model.api.IDataSource
+import com.example.githubclient.mvp.model.entity.ReposGitHubUser
+import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class RetrofitGitHubUsersRepo(val api: IDataSource) : IGitHubUsersRepo {
     override fun getUsers() = api.loadUsers().subscribeOn(Schedulers.io())
+    override fun getReposUser(url:String)= api.getReposUser(url).subscribeOn(Schedulers.io())
 }
