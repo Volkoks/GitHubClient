2b05b22dd0b3de382332ec94a3b137d35ed82f68
Volkoks <volk-2390@gmail.com>
2020-11-24 09:43:56 +0300

    Добавил создание экрана в навигаторе

--- app/src/main/java/com/example/githubclient/navigator/Screens.kt
+++ app/src/main/java/com/example/githubclient/navigator/Screens.kt
@@ -1,6 +1,9 @@
 package com.example.githubclient.navigator
 
+import androidx.fragment.app.Fragment
 import com.example.githubclient.mvp.model.entity.GitHubUser
+import com.example.githubclient.mvp.model.entity.ReposGitHubUser
+import com.example.githubclient.ui.fragments.ReposFragment
 import com.example.githubclient.ui.fragments.UserFragment
 import com.example.githubclient.ui.fragments.UsersFragment
 import ru.terrakok.cicerone.android.support.SupportAppScreen
@@ -9,7 +12,12 @@ class Screens {
     class UsersScreen : SupportAppScreen() {
         override fun getFragment() = UsersFragment.newInstance()
     }
-    class UserScreen(val user: GitHubUser): SupportAppScreen(){
+
+    class UserScreen(val user: GitHubUser) : SupportAppScreen() {
         override fun getFragment() = UserFragment.newInstance(user)
+    }
+
+    class ReposScreen(val reposUser: ReposGitHubUser) : SupportAppScreen() {
+        override fun getFragment() = ReposFragment.newInstance(reposUser)
     }
 }
